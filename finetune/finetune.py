b51820753989a7eeecd2bc3eb55b445335185054
finallymint <973426574@qq.com>
2025-01-05 23:37:01 +0800

    initial commit

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -1,56 +1,128 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2021 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Fine-tuning the library models for sequence to sequence.
+"""
+# You can also adapt this script on your own sequence to sequence task. Pointers for this are left as comments.
+# Adapted from
 
-# 目标URL
-url = "https://directory.seas.upenn.edu/"
 
-# 发送请求获取网页内容
-response = requests.get(url)
+import logging
+import os
+import sys
+import json
+import transformers
+from transformers import (
+    AutoTokenizer,
+    HfArgumentParser,
+    Seq2SeqTrainingArguments,
+    set_seed,
+)
 
-# 检查请求是否成功
-if response.status_code == 200:
-    # 使用BeautifulSoup解析HTML文档
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # 查找所有包含StaffListMeta类的div元素
-    staff_list_meta = soup.find_all('div', class_='StaffListMeta')
+from arguments import ModelArguments, DataTrainingArguments
 
-    # 创建列表以保存所有工作人员的数据
-    staff_data = []
+from preprocess_utils import sanity_check, InputOutputDataset
 
-    for staff in staff_list_meta:
-        # 提取姓名和对应的个人主页链接
-        name_link = staff.find('div', class_='StaffListName').find('a', href=True)
-        name = name_link.get_text(strip=True)
-        personal_page = name_link['href']
-        
-        # 提取职位和部门
-        titles_div = staff.find('div', class_='StaffListTitles')
-        titles = [title.get_text(strip=True) for title in titles_div.find_all('div')] if titles_div else []
-        
-        # 提取特别职务
-        special_title = staff.find('div', class_='StaffListSpecialTitle').get_text(strip=True) if staff.find('div', class_='StaffListSpecialTitle') else None
-        
-        # 提取电子邮件链接
-        email_link = staff.find('a', href=True)['href'] if staff.find('a', href=True) and 'mailto:' in staff.find('a', href=True)['href'] else None
-        
-        # 将信息添加到列表中
-        staff_data.append({
-            '姓名': name,
-            '个人主页': personal_page,
-            '职位和部门': ' | '.join(titles),
-            '特别职务': special_title or '无',
-            '电子邮件': email_link
-        })
-    
-    # 创建数据框
-    df = pd.DataFrame(staff_data)
+logger = logging.getLogger(__name__)
 
-    # 将数据框保存为Excel文件
-    excel_file_path = 'staff_list_with_personal_pages.xlsx'
-    df.to_excel(excel_file_path, index=False)
-    
-    print(f"数据已成功保存到 {excel_file_path}")
-else:
-    print("无法获取网页内容，状态码:", response.status_code)
+
+def main():
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments)
+    )
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(
+            json_file=os.path.abspath(sys.argv[1])
+        )
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    if training_args.should_log:
+        # The default of training_args.log_level is passive, so we set log level at info here to have that default.
+        transformers.utils.logging.set_verbosity_info()
+
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    # datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.bf16}"
+    )
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+    # Load pretrained model and tokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path, trust_remote_code=True
+    )
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path, trust_remote_code=True
+        )
+    tokenizer.pad_token = tokenizer.eos_token
+
+    with open(data_args.train_file, "r", encoding="utf-8") as f:
+        if data_args.train_file.endswith(".json"):
+            train_data = json.load(f)
+        elif data_args.train_file.endswith(".jsonl"):
+            train_data = [json.loads(line) for line in f]
+
+    train_dataset = InputOutputDataset(
+        train_data,
+        tokenizer,
+        data_args.max_source_length,
+        data_args.max_target_length,
+    )
+    if training_args.local_rank < 1:
+        sanity_check(train_dataset[0]["input_ids"], train_dataset[0]["labels"])
+
+    # Initialize our Trainer
+    trainer = transformers.Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        tokenizer=tokenizer,
+    )
+
+    checkpoint = None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    model.gradient_checkpointing_enable()
+    model.enable_input_require_grads()
+    trainer.train(resume_from_checkpoint=checkpoint)
+    trainer.save_model()  # Saves the tokenizer too for easy upload
+    trainer.save_state()
+
+
+if __name__ == "__main__":
+    main()
